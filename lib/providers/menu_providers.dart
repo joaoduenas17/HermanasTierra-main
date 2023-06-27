f796665b0ff7219b38ce8f7eca196cc43b533b35
JuanJoseArt <115014021+JuanJoseArt@users.noreply.github.com>
2023-06-27 00:08:41 -0500

    primero

--- lib/providers/menu_providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MenuProviders extends ChangeNotifier {
-  int itemselect = 0;
-  setselect(int i) {
-    itemselect = i;
-    notifyListeners();
-  }
-}
